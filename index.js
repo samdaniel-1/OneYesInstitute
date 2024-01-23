c0121425020396e27aa0438a4238f26ce36b352d
samdaniel-1 <81462039+samdaniel-1@users.noreply.github.com>
2024-01-23 18:23:51 +0530

    Express.Js

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const port = 5010;
+const app = express()
+app.use(express.json());
+app.use(cors())
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "India@2023",
+    database: "zi"
+    
+})
+db.connect((err,result)=>{
+    if(err)
+    console.log("err occured ",err);
+    else
+    console.log("DB connected ");
+})
+let geti=0;
+app.post("/insert",(req,res)=>{
+    const {name, number, report, pincode} = req.body;
+    let insertion = "insert into User(name, phone, report, pincode) value (?,?,?,?);";
+    
+    db.query(insertion,[name,number, report, pincode],(err,result)=>{
+        if(err)
+        console.log("Hi error there",err);
+        else
+        res.send({status: "inserted"});
+        console.log(geti)
+    })
+})
+app.get("/view",(req,res)=>{
+    let select = "select * from User";
+    db.query(select,(err,result)=>{
+        if(err)
+        console.log("Error there",err)
+    else
+    console.log(result)
+    res.send(result)
+    })
+})
+app.listen(port,()=>{
+    console.log("Connected to the port",port);
+});
